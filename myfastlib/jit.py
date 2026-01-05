c30a6c70110d5734da696cf4d007e077fa858d3b
GitХубщик <kapostall81@gmail.com>
2026-01-05 13:23:29 +0300

    Update jit.py

--- myfastlib/jit.py
+++ myfastlib/jit.py
@@ -1,6 +1,5 @@
 import ast
 
-
 class DeadCodeRemover(ast.NodeTransformer):
 """Простейшая вырезка `if False:` блоков и `if 0:`"""
 def visit_If(self, node):
@@ -15,21 +14,14 @@ return node
 
 _jit_cache = {}
 
-
-
-
 def _find_function_node(mod, name):
 for node in ast.walk(mod):
 if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)) and node.name == name:
 return node
 return None
 
-
-
-
 def jit_optimize(func=None):
 """Декоратор: оптимизирует и компилирует функцию на лету.
-
 
 Применение:
 @jit_optimize
@@ -52,11 +44,9 @@ except (OSError, IOError, TypeError):
 # нельзя получить исходник — возвращаем оригинал
 return func
 
-
 key = (func.__module__, func.__qualname__, source)
 if key in _jit_cache:
 return _jit_cache[key]
-
 
 try:
 # парсим и оптимизируем AST
@@ -65,13 +55,11 @@ func_node = _find_function_node(mod, func.__name__)
 if func_node is None:
 return func
 
-
 folder = ConstantFolder()
 dcr = DeadCodeRemover()
 folder.visit(mod)
 dcr.visit(mod)
 ast.fix_missing_locations(mod)
-
 
 # компилируем оптимизированный модуль в новый namespace
 globs = func.__globals__.copy()
@@ -82,15 +70,14 @@ new_func = ns.get(func.__name__)
 if new_func is None:
 return func
 
-
 try:
 functools.update_wrapper(new_func, func)
 except Exception:
 pass
-
 
 _jit_cache[key] = new_func
 return new_func
 except Exception:
 # в случае любой ошибки возвращаем исходную функцию
 return func
+
